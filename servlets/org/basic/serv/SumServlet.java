2ca642ffb20b340079ac1b15b223ab5e77f54b37
bhaskerath <bhaskerathikayala@gmail.com>
2021-06-05 18:47:17 +0530

    added source files

---
+++ servlets/org/basic/serv/SumServlet.java
@@ -0,0 +1,30 @@
+package org.basic.serv;
+
+import java.io.IOException; 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SumServlet
+ */
+@WebServlet("sum")
+public class SumServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String a = request.getParameter("a");
+		String b = request.getParameter("b");
+		
+		response.getWriter().print(" data : "+a+" ; "+b);
+		
+	}
+
+}
